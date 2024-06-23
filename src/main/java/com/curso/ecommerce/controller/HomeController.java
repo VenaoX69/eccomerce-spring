0d74c0831c80f2e174edfb08cf238e28a1300440
VenaoX69 <danielhr090@gmail.com>
2024-06-22 19:56:34 -0500

    Se creo el metodo para mostrar la template de la información de los productos em (HomeController.java) y se agrego el metodo al html(productohome.html)

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.curso.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.service.ProductoService;
@@ -11,6 +14,9 @@ import com.curso.ecommerce.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log = LoggerFactory.getLogger(HomeController.class); // "import org.slf4j.Logger;", "import org.slf4j.LoggerFactory;", esta variable logger se hace con el fin de hacer pruebas/testeos de lo que se tiene en las variables.
+	
 	@Autowired // Esta anotación para que lo inyecte al controlador del framework
 	private ProductoService productoService;
 	
@@ -21,5 +27,14 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll()); // El metodo findAll, trae todos los productos existentes.
 		
 		return "usuario/home";
+	}
+	
+	// Metodo para llevar al usuario a ver la informacion del producto
+	@GetMapping("productohome/{id}") // El método va a resporder a peticiones de tipo get; y se va a llamar "productohome" y va a tener un parametro "{id}" para poder usarlo.
+	public String productoHome(@PathVariable Integer id) { // Se usa "@PathVariable" para que tome la variable del "GetMapping".
+		
+		log.info("Id producto enviado como parámetro {}", id);
+		
+		return "usuario/productohome";
 	}
 }
